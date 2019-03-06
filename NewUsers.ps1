8f93923c034d4f3973c6bf49f36871fdab9631af
Mathew Ealy <mealy58@gmail.com>
2019-03-06 15:02:08 -0500

    spelling error needed fixed

--- NewUsers.ps1
+++ NewUsers.ps1
@@ -7,7 +7,7 @@ You can find the ProfileID in your system on table "Profile Table (2000000072)"
 
 #Import the NavAdmin Module
 Import-Module “${env:ProgramFiles}\Microsoft Dynamics NAV\110\Service\NAVAdminTool.ps1”
-#Nav version changes the 110 to a differnt number
+#Nav version changes the 110 to a different number
 #Import the CSV file **Change the -path to the file
 $Users = Import-CSV -path "\\server\share\file.csv"
 #Variables
