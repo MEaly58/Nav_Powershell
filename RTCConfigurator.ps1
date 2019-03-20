a59ce62042d44e14b7036b9cd96d7357a83b66f1
Mathew Ealy <mealy58@gmail.com>
2019-03-20 17:00:54 -0400

    Update RTCConfigurator.ps1

--- RTCConfigurator.ps1
+++ RTCConfigurator.ps1
@@ -3,7 +3,7 @@
 #Promt user for Role to edit
 $Profile = Read-Host -Promt "What Role would you like to edit?"
 #Change to the Dynamics Nav dirctory
-cd "${env:ProgramFiles}\Microsoft Dynamics NAV\60\RoleTailored Client"
-cd ./Microsoft.Dynamics.Nav.Client.exe -configure -profile:"$profile"
+cd "${env:ProgramFiles}\Microsoft Dynamics NAV\110\RoleTailored Client"
+./Microsoft.Dynamics.Nav.Client.exe -configure -profile:"$profile"
 #Make your changes to the profile
 #https://msdn.microsoft.com/en-us/library/dd354992.aspx
