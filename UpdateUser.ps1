e96922afea97233d341180d198bcdbaa1f68f478
Mathew Ealy <mealy58@gmail.com>
2019-11-27 14:55:13 -0500

    Create UpdateUser.ps1

---
+++ UpdateUser.ps1
@@ -0,0 +1,25 @@
+#CSV format
+<#!
+FullName,Login,Email,Profile, 
+You can find the ProfileID in your system on table "Profile Table (2000000072)"
+#> 
+
+
+#Import the NavAdmin Module
+Import-Module “${env:ProgramFiles}\Microsoft Dynamics NAV\110\Service\NAVAdminTool.ps1”
+#Nav version changes the 110 to a different number
+#Import the CSV file **Change the -path to the file
+$Users = Import-CSV -path "\\server\share\file.csv"
+#Variables
+$InstanceName = "NavInstance"
+#Execute script
+foreach ($User in $Users)
+{
+$FullName = $User.'FullName'
+$Login = $User.'Login'
+$Email = $User.'Email'
+$Profile = $User.'Profile'
+
+Set-NavServerUser -ServerInstance $InstanceName -WindowsAccount $Login -FullName "$FullName" -ContactEmail "$Email" -ProfileID "$Profile" -Company "CompanyName" -LanguageId 1033
+
+}
