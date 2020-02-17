299c005d6b55f52ff05bc7de0ee82d3168ad232f
Mathew Ealy <mealy58@gmail.com>
2020-02-17 09:19:14 -0500

    Update RestartServerInstance.ps1

--- RestartServerInstance.ps1
+++ RestartServerInstance.ps1
@@ -1,6 +1,6 @@
 <# Import Modules #>
-Import-Module "${env:ProgramFiles}\Microsoft Dynamics NAV\90\Service\Microsoft.Dynamics.Nav.Model.Tools.psd1' -WarningAction SilentlyContinue | out-null
-Import-Module "${env:ProgramFiles}\Microsoft Dynamics NAV\90\Service\NavAdminTool.ps1' -WarningAction SilentlyContinue | Out-Null
+Import-Module "${env:ProgramFiles}\Microsoft Dynamics NAV\90\Service\Microsoft.Dynamics.Nav.Model.Tools.psd1" -WarningAction SilentlyContinue | out-null
+Import-Module "${env:ProgramFiles}\Microsoft Dynamics NAV\90\Service\NavAdminTool.ps1" -WarningAction SilentlyContinue | Out-Null
 <# Run against Dynamics NAV servers running 16 #>
 $Instances = Get-NAVServerInstance
 
