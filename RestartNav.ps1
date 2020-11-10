68d909db96c3dd2ea9337598b6bb296c73b3235b
Mathew Ealy <Mathew.Ealy@Rentokil.com>
2020-11-10 11:52:20 -0500

    finished script

--- RestartNav.ps1
+++ RestartNav.ps1
@@ -1,4 +1,7 @@
 #MicrosoftDynamicsNavServer$DynamicsNAV110
 $Service = 'MicrosoftDynamicsNavServer$DynamicsNAV110'
 $Computers = Get-Content "C:\Path\File.txt"
+foreach ($Computer in $Computers)
+{
 Invoke-Command -ComputerName $Computers {Restart-Service $Service}
+}
