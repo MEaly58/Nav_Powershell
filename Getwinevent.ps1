f2f09c81675cc57bb924ecebf03388d06c6e1188
MEly58 <Mathew.Ealy@Rentokil.com>
2020-11-13 11:55:55 -0500

    moved to proper project

--- Getwinevent.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$computerName = 'localhost' ,[datetime]$StartTimestamp, [datetime]$EndTimestamp)
-
-$Logs = (Get-WinEvent -ListLog -ComputerName $ComputerName | where { $_.RecordCount}).LogName
-$FilterTable = @{
-  'StartTime' = $StartTimestamp
-  'EndTime' = $EndTimestamp
-  'LogName' = $Logs
-}
-Get-WinEvent -ComputerName $ComputerName -FilterHashtable $FilterTable -ErrorAction 'SilentlyContinue'
