e3f1d4732c5720e1eefea15a21222da3de99cd36
MEaly58 <Mealy58@gmail.com>
2021-04-16 14:43:54 -0400

    updated to use veriables

--- NewCompany.ps1
+++ NewCompany.ps1
@@ -1,5 +1,8 @@
 #Import the NavAdmin Module
 Import-Module “${env:ProgramFiles}\Microsoft Dynamics NAV\110\Service\NAVAdminTool.ps1”
 #Nav version changes the 110 to a differnt number
-#Replace $instanceName with your service instance name 
-New-NAVCompany -ServerInstance '$instanceName' -Tenant CRONUS -CompanyName 'CRONUS Subsidiary'
+#Veriables
+$instanceName = YourInstinceName
+$CompanyName = CompanyNameToRemove
+#Replace $instanceName with your service instance name
+New-NAVCompany -ServerInstance '$instanceName' -Tenant CRONUS -CompanyName '$CompanyName'
